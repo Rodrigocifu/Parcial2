6b69791fbbcec462b830919929f5c2e0bd66f247
Rodrigocifu <128503236+Rodrigocifu@users.noreply.github.com>
2023-03-21 09:52:27 -0600

    Create mayormenor.py

---
+++ mayormenor.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingrese el primero número: "))
+numero2 = int(input("Ingrese el segundo número: "))
+if numero1 > numero2:
+    print("número 1 es mayor a número 2")
+elif numero1 < numero2:
+    print("Número 2 es mayor a número 1")
+
